ebd1aee7c98817e5309bb6657e9b34a4019811b7
Juan Barbat <barbat.juan@gmail.com>
2024-10-22 12:41:22 -0300

    carrito mas lindo

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -6,175 +6,78 @@ const CartWidget = () => {
   return (
     <a className="nav-link-custom" href="">
       <svg
-        clip-rule="evenodd"
-        fill-rule="evenodd"
-        height="50"
-        image-rendering="optimizeQuality"
-        shape-rendering="geometricPrecision"
-        text-rendering="geometricPrecision"
-        viewBox="0 0 900 900"
-        width="50"
+        id="Layer_25"
+        height="40"
+        viewBox="0 0 32 32"
+        width="40"
         xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
+        data-name="Layer 25"
         version="1.1"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
         xmlns:svgjs="http://svgjs.dev/svgjs"
       >
         <g transform="matrix(1,0,0,1,0,0)">
-          <linearGradient
-            id="id0"
-            gradientUnits="userSpaceOnUse"
-            x1="902.19"
-            x2="-2.19"
-            y1="497.063"
-            y2="402.937"
-          >
-            <stop offset="0" stop-color="#1e8ac6" />
-            <stop offset=".301961" stop-color="#834fbc" />
-            <stop offset="1" stop-color="#e813b1" />
-          </linearGradient>
-          <g id="Layer_x0020_1">
-            <circle
-              cx="450"
-              cy="450"
+          <path
+            d="m24.271 22h-16.408a2.056 2.056 0 0 1 -2.063-1.717l-1.782-10a2.028 2.028 0 0 1 2.062-2.283h21.839a2.017 2.017 0 0 1 2 2.549l-3.648 10a2.07 2.07 0 0 1 -2 1.451z"
+            fill="#fbd9ad"
+            fill-opacity="1"
+            data-original-color="#b3f8ffff"
+            stroke="none"
+            stroke-opacity="1"
+          />
+          <circle
+            cx="8"
+            cy="28"
+            fill="#fbd9ad"
+            r="2"
+            fill-opacity="1"
+            data-original-color="#b3f8ffff"
+            stroke="none"
+            stroke-opacity="1"
+          />
+          <circle
+            cx="24"
+            cy="28"
+            fill="#fbd9ad"
+            r="2"
+            fill-opacity="1"
+            data-original-color="#b3f8ffff"
+            stroke="none"
+            stroke-opacity="1"
+          />
+          <g fill="#046bba">
+            <path
+              d="m30 25h-26a1 1 0 0 1 0-2h20.271a3.065 3.065 0 0 0 2.939-2.107l3.667-10.059a2.918 2.918 0 0 0 -.467-2.6 3.076 3.076 0 0 0 -2.491-1.234h-21.072l-.861-5.165a1 1 0 0 0 -.986-.835h-3a1 1 0 0 0 0 2h2.153l.709 4.253a3.061 3.061 0 0 0 -1.14.818 2.96 2.96 0 0 0 -.685 2.387l1.777 9.973a2.886 2.886 0 0 0 .149.569h-.963a3 3 0 0 0 0 6h1.184a2.965 2.965 0 0 0 -.184 1 3 3 0 0 0 6 0 2.965 2.965 0 0 0 -.184-1h10.368a2.965 2.965 0 0 0 -.184 1 3 3 0 0 0 6 0 2.965 2.965 0 0 0 -.184-1h3.184a1 1 0 0 0 0-2zm-23.212-4.893-1.777-9.971a.936.936 0 0 1 .226-.759 1.109 1.109 0 0 1 .844-.377h21.838a1.093 1.093 0 0 1 .889.433.9.9 0 0 1 .172.774l-3.667 10.059a1.073 1.073 0 0 1 -1.042.734h-16.408a1.067 1.067 0 0 1 -1.075-.893zm2.212 7.893a1 1 0 1 1 -1-1 1 1 0 0 1 1 1zm16 0a1 1 0 1 1 -1-1 1 1 0 0 1 1 1z"
               fill="#c99e67"
-              r="450"
-              data-original-color="id0"
+              fill-opacity="1"
+              data-original-color="#046bbaff"
               stroke="none"
               stroke-opacity="1"
-              fill-opacity="1"
             />
-            <g id="_346410936">
-              <g id="_346410096">
-                <path
-                  d="m305 320c-3 0-5-2-6-4l-25-96h-74c-3 0-6-2-6-6 0-3 3-6 6-6h78c3 0 5 2 6 5l27 99c0 4-1 7-5 8z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346410960">
-                <path
-                  d="m377 558c-3 0-5-2-6-4l-72-238c0-2 0-4 1-6 1-1 3-2 5-2h379c7 0 14 4 18 9 4 4 5 9 3 14l-48 166c-3 9-9 12-14 13l-265 48zm-64-238 68 225 260-47c2 0 3-1 4-5l49-166s0-1-1-3c-2-2-5-4-9-4z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411200">
-                <path
-                  d="m397 532c-2 0-5-1-5-4l-58-189c0-1 0-3 1-5 1-1 3-2 5-2h329c2 0 3 1 5 2 1 2 1 4 1 5l-43 147c-1 3-3 4-5 5l-229 41s0 0-1 0zm-49-188 54 175 219-40 40-135z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346410792">
-                <path
-                  d="m634 648h-13c-3 0-6-2-6-6 0-3 3-6 6-6h37c3 0 6 3 6 6 0 4-3 6-6 6zm-99 0h-81c-3 0-6-2-6-6 0-3 3-6 6-6h81c3 0 6 3 6 6 0 4-3 6-6 6zm-167 0h-5c-4 0-7 0-11-1-22-5-38-25-38-48s17-41 47-49l14-4 4 12-15 4c-14 3-38 13-38 37 0 17 12 33 29 36 2 1 5 1 8 1h5c3 0 6 3 6 6 0 4-3 6-6 6z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411008">
-                <path
-                  d="m578 692c-27 0-49-23-49-50s22-49 49-49 49 22 49 49-22 50-49 50zm0-87c-20 0-37 17-37 37 0 21 17 38 37 38 21 0 38-17 38-38 0-20-17-37-38-37z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411512">
-                <path
-                  d="m411 692c-27 0-49-23-49-50s22-49 49-49 49 22 49 49-22 50-49 50zm0-87c-21 0-37 17-37 37 0 21 16 38 37 38 20 0 37-17 37-38 0-20-17-37-37-37z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411728">
-                <path
-                  d="m578 669c-14 0-26-12-26-27 0-14 12-26 26-26 15 0 27 12 27 26 0 15-12 27-27 27zm0-41c-8 0-14 6-14 14s6 15 14 15 15-7 15-15-7-14-15-14z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411536">
-                <path
-                  d="m411 669c-15 0-27-12-27-27 0-14 12-26 27-26 14 0 26 12 26 26 0 15-12 27-26 27zm0-41c-8 0-15 6-15 14s7 15 15 15 14-7 14-15-6-14-14-14z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411584">
-                <path
-                  d="m359 407c-3 0-5-2-6-5 0-3 3-6 6-7l297-21c3 0 6 2 6 5 1 4-2 7-5 7l-297 21s0 0-1 0z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411320">
-                <path
-                  d="m376 461c-3 0-6-2-6-6-1-3 2-6 5-6l265-19c3 0 6 2 6 5 0 4-2 7-5 7z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411752">
-                <path
-                  d="m438 456c-3 0-6-2-6-6v-54c0-4 3-6 6-6 4 0 6 2 6 6v54c0 4-2 6-6 6z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411560">
-                <path
-                  d="m512 451c-3 0-6-3-6-6v-55c0-3 3-6 6-6s6 3 6 6v55c0 3-3 6-6 6z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-              <g id="_346411392">
-                <path
-                  d="m586 446c-3 0-6-3-6-6v-55c0-3 3-6 6-6s6 3 6 6v55c0 3-3 6-6 6z"
-                  fill="#2d2d2d"
-                  fill-opacity="1"
-                  data-original-color="#ffffffff"
-                  stroke="none"
-                  stroke-opacity="1"
-                />
-              </g>
-            </g>
+            <path
+              d="m16 19a1 1 0 0 0 1-1v-6a1 1 0 0 0 -2 0v6a1 1 0 0 0 1 1z"
+              fill="#c99e67"
+              fill-opacity="1"
+              data-original-color="#046bbaff"
+              stroke="none"
+              stroke-opacity="1"
+            />
+            <path
+              d="m21 19a1 1 0 0 0 1-1v-6a1 1 0 0 0 -2 0v6a1 1 0 0 0 1 1z"
+              fill="#c99e67"
+              fill-opacity="1"
+              data-original-color="#046bbaff"
+              stroke="none"
+              stroke-opacity="1"
+            />
+            <path
+              d="m11 19a1 1 0 0 0 1-1v-6a1 1 0 0 0 -2 0v6a1 1 0 0 0 1 1z"
+              fill="#c99e67"
+              fill-opacity="1"
+              data-original-color="#046bbaff"
+              stroke="none"
+              stroke-opacity="1"
+            />
           </g>
         </g>
       </svg>
