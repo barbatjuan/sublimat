e7e02f78ed332103be68ea773720477c09b83134
Juan Barbat <barbat.juan@gmail.com>
2024-11-09 12:18:53 -0300

    segunda renderizacion

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,25 +1,44 @@
-import React, { useEffect, useState } from "react";
-import asyncMock from "./../../asyncMock";
+import React from "react";
+import { fetchProductos } from "./../../asyncMock";
+import "./ItemListContainer.css";
 
-const ItemListContainer = ({ categoryId }) => {
-  const [products, setProducts] = useState([]);
+const ItemListContainer = () => {
+  const [productos, setProductos] = React.useState([]);
 
-  useEffect(() => {
-    // Filtrar productos por categoría si categoryId está disponible
-    const filteredProducts = asyncMock.filter(
-      (product) => !categoryId || product.category === categoryId
-    );
-    setProducts(filteredProducts);
-  }, [categoryId]);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchProductos();
+      setProductos(data);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <img src={product.img} alt={product.name} />
-          <p>{product.description}</p>
-          <p>Precio: ${product.price}</p>
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {productos.map((producto) => (
+        <div
+          key={producto.id}
+          className="card bg-white shadow-lg rounded-lg overflow-hidden"
+        >
+          <img
+            src={producto.img}
+            alt={producto.name}
+            className="card-img w-full h-64 object-cover"
+          />
+          <div className="card-body p-4">
+            <h3 className="text-gray-900 font-semibold text-xl">
+              {producto.name}
+            </h3>
+            <p className="text-gray-700 mt-2">{producto.description}</p>
+            <div className="card-footer flex justify-between items-center mt-4">
+              <span className="text-gray-900 font-semibold">
+                ${producto.price}
+              </span>
+              <button className="btn-add  text-white py-2 px-4 rounded ">
+                Agregar
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
